caad72682541d70c4a61958ed54eb1ec9a12d313
kof <zhkefu@gmail.com>
2016-03-10 10:49:58 +0800

    新增数据库帮助类，以及各表对应的实体类

---
+++ app/src/main/java/com/kof/weather/model/Province.java
@@ -0,0 +1,34 @@
+package com.kof.weather.model;
+
+/**
+ * Created by Kof on 2016/3/9.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getProvinceName(){
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName){
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode(){
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode){
+        this.provinceCode = provinceCode;
+    }
+}
